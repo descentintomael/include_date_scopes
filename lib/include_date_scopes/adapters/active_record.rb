fffcaa13ad4807989cc453ab36160a3bc68f2905
Sean Todd <sean@paynearme.com>
2014-04-11 09:43:57 -0700

    This limitation isn't necessary

--- lib/include_date_scopes/adapters/active_record.rb
+++
@@ -1,9 +0,0 @@
-# Shamelessly stolen from the squeel gem.
-require 'active_record'
-
-case ActiveRecord::VERSION::MAJOR
-when 3
-  ActiveRecord::Base.send :extend, IncludeDateScopes
-else
-  raise NotImplementedError, "include_date_scopes does not support Active Record version #{ActiveRecord::VERSION::STRING}"
-end
