13d412626202212788490f687a4319040ef0dfcc
Katharina Peter <gaz@gmx.net>
2014-09-12 17:50:12 -0700

    combined some methods in define_timestamp_scopes

--- lib/include_date_scopes/define_timestamp_scopes.rb
+++ lib/include_date_scopes/define_timestamp_scopes.rb
@@ -1,6 +1,7 @@
 
 module IncludeDateScopes
   module DefineTimestampScopes
+ 
     def define_timestamp_scopes_for(column_name, prepend_name = false)
       prefix = prepend_name ? "#{column_name}_" : ""
       t = self.arel_table
@@ -16,11 +17,11 @@ module IncludeDateScopes
       end
 
       define_singleton_method :"#{prefix}on_or_before" do |time| 
-        where(t[column_name].lteq (time.is_a?(Date) ? time.to_time : time))
+        where(t[column_name].lteq time_object(time))
       end
 
       define_singleton_method :"#{prefix}before" do |time| 
-        where(t[column_name].lt (time.is_a?(Date) ? time.to_time : time))
+        where(t[column_name].lt time_object(time))
       end
 
       define_singleton_method :"#{prefix}on_or_after_date" do |time|
@@ -28,7 +29,7 @@ module IncludeDateScopes
       end
 
       define_singleton_method :"#{prefix}on_or_after" do |time|
-        where(t[column_name].gteq (time.is_a?(Date) ? time.to_time + 1.day : time ))
+        where(t[column_name].gteq time_object(time))
       end
 
       define_singleton_method :"#{prefix}after" do |time|
@@ -47,72 +48,45 @@ module IncludeDateScopes
         __send__(:"#{prefix}last_day")
       end
 
-      define_singleton_method :"#{prefix}next_minute" do
-        __send__(:"#{prefix}between", Time.now, 1.minute.from_now)
+      [:minute, :hour].each do |time_unit|
+        define_singleton_method :"#{prefix}next_#{time_unit}" do
+         __send__(:"#{prefix}between", Time.now, 1.send(time_unit).from_now)
+        end
+
+        define_singleton_method :"#{prefix}last_#{time_unit}" do
+          __send__(:"#{prefix}between", 1.send(time_unit).ago, Time.now)
+        end
       end
 
-      define_singleton_method :"#{prefix}next_hour" do
-        __send__(:"#{prefix}between", Time.now, 1.hour.from_now)
-      end
+      [:seconds, :minutes, :hours].each do |time_unit|
+        define_singleton_method :"#{prefix}last_n_#{time_unit}" do |count|
+          __send__(:"#{prefix}between", count.send(time_unit).ago, Time.now)
+        end
 
-      define_singleton_method :"#{prefix}last_minute" do
-        __send__(:"#{prefix}between", 1.minute.ago, Time.now)
-      end
-
-      define_singleton_method :"#{prefix}last_hour" do
-        __send__(:"#{prefix}between", 1.hour.ago, Time.now)
-      end
-
-      define_singleton_method :"#{prefix}last_n_seconds" do |count|
-        __send__(:"#{prefix}between", count.seconds.ago, Time.now)
-      end
-
-      define_singleton_method :"#{prefix}last_n_minutes" do |count|
-        __send__(:"#{prefix}between", count.minutes.ago, Time.now)
-      end
-
-      define_singleton_method :"#{prefix}last_n_hours" do |count|
-        __send__(:"#{prefix}between", count.hours.ago, Time.now)
-      end
-
-      define_singleton_method :"#{prefix}next_n_seconds" do |count|
-        __send__(:"#{prefix}between", Time.now, count.seconds.from_now)
-      end
-
-      define_singleton_method :"#{prefix}next_n_minutes" do |count|
-        __send__(:"#{prefix}between", Time.now, count.minutes.from_now)
-      end
-
-      define_singleton_method :"#{prefix}next_n_hours" do |count|
-        __send__(:"#{prefix}between", Time.now, count.hours.from_now)
+        define_singleton_method :"#{prefix}next_n_#{time_unit}" do |count|
+          __send__(:"#{prefix}between", Time.now, count.send(time_unit).from_now)
+        end
       end
 
       define_singleton_method :"#{prefix}this_minute" do
         __send__(:"#{prefix}between", Time.now.change(sec: 0), Time.now.change(sec: 59, usec: Rational(999999999, 1000)))
       end
 
-      define_singleton_method :"#{prefix}this_hour" do
-        __send__(:"#{prefix}between", 1.hour.ago.end_of_hour, Time.now.end_of_hour)
-      end
-
-      define_singleton_method :"#{prefix}this_day" do
-        __send__(:"#{prefix}between", 1.day.ago.end_of_day, Time.now.end_of_day)
-      end
-
-      define_singleton_method :"#{prefix}this_week" do
-        __send__(:"#{prefix}between", 1.week.ago.end_of_week, Time.now.end_of_week)
-      end
-
-      define_singleton_method :"#{prefix}this_month" do
-        __send__(:"#{prefix}between", 1.month.ago.end_of_month, Time.now.end_of_month)
-      end
-
-      define_singleton_method :"#{prefix}this_year" do
-        __send__(:"#{prefix}between", 1.year.ago.end_of_year, Time.now.end_of_year)
+      [:hour, :day, :week, :month, :year].each do |time_unit|
+        define_singleton_method :"#{prefix}this_#{time_unit}" do
+          __send__(:"#{prefix}between", 1.send(time_unit).ago.send(:"end_of_#{time_unit}"), Time.now.send(:"end_of_#{time_unit}"))
+        end
       end
 
       define_common_scopes t, prefix, column_name
     end
-  end
+
+    private
+
+      def time_object(time)
+        time.is_a?(Date) ? time.to_time + 1.day : time
+      end
+      
+   end
 end
 
